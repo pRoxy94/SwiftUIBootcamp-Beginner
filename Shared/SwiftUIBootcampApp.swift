20edb5cd099bef3a1ae7b8e0ec5e81fd3a7025c0
Rossana Laricchia <rossana.laricchia@eng.it>
2022-05-06 10:46:20 +0200

    Initial Commit

---
+++ Shared/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  Shared
+//
+//  Created by Rossana Laricchia on 06/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
